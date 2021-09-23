e485f2f6a3ecef80ec76ac769dbdbf1e2b981104
Anna Haskelsky <annahaskelsky@gmail.com>
2021-09-23 15:57:51 +0300

    started card edit

--- src/cmps/card-edit.jsx
+++ src/cmps/card-edit.jsx
@@ -1,19 +1,46 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { onEditBoard } from '../store/board.actions'
 
-export class CardEdit extends Component {
+class _CardEdit extends Component {
     state = {
         currCard: ''
     }
 
+    getCardById = (cardId) => {
+        const cards = this.props.board.cards
+        return card = cards.find(card => card.id === cardId)
+    }
+
     componentDidMount() {
-        const cardId = this.props.match.params.cardId
+        let currCard
+        if (this.props.card) currCard = this.props.card
+        else {
+            const { cardId } = this.props.match.params.cardId
+            currCard = this.getCardById(cardId)
+        }
+        this.setState({ currCard })
     }
 
     render() {
+        const { currCard } = this.state
+        if (!currCard) return <></>
         return (
-            <div>
-
+            <div className="card-edit">
+                {currCard.style.bgColor && <div className="card-bg" style={{backgroundColor=currCard.style.bgColor}}></div>}
             </div>
         )
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        board: state.boardModule.currBoard
+    }
+}
+
+const mapDispatchToProps = {
+    onEditBoard
+}
+
+export const CardEdit = connect(mapStateToProps, mapDispatchToProps)(_CardEdit);
