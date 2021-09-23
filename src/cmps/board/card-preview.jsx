71785a9d5608e2dcab71cce9c97c0fd60683abf0
ShirLaV <shir0lavi@gmail.com>
2021-09-23 15:46:15 +0300

    fonts

--- src/cmps/board/card-preview.jsx
+++ src/cmps/board/card-preview.jsx
@@ -1,9 +1,10 @@
+import { HiOutlinePencil  } from 'react-icons/hi';
 
 export function CardPreview({ card }) {
   return (
     <div className='card-preview flex space-between'>
       <p>{card.title}</p>
-      <button className="hover-edit-btn">edit</button>
+      <button className="hover-edit-btn"><HiOutlinePencil /></button>
     </div>
   );
 }
