d4148ab970f1efdd94dfa05b8a27a91d7b0278c6
ShirLaV <shir0lavi@gmail.com>
2021-09-24 19:02:22 +0300

    scss and store changes

--- src/cmps/board/card-preview.jsx
+++ src/cmps/board/card-preview.jsx
@@ -1,55 +1,120 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { HiOutlinePencil } from 'react-icons/hi';
-import {GrTextAlignFull} from 'react-icons/gr'
+import { GrTextAlignFull } from 'react-icons/gr';
+import { GrCheckbox } from 'react-icons/gr';
+import { GrCheckboxSelected } from 'react-icons/gr';
+import { FiClock } from 'react-icons/fi';
 
+import {onUpdateCard} from '../../store/board.actions.js'
+import { MemberAvatar } from '../shared/member-avatar.jsx';
 class _CardPreview extends Component {
   state = {
     isCardLabelListOpen: false,
   };
 
-  toggleCardLabelList = () => {
+  toggleCardLabelList = (event) => {
+    event.stopPropagation();
     this.setState({ isCardLabelListOpen: !this.state.isCardLabelListOpen });
   };
 
   getLabel = (labelId) => {
     const board = this.props.board;
-    const label=board.labels.find(label=> label.id===labelId)
-    return label
+    const label = board.labels.find((label) => label.id === labelId);
+    return label;
   };
 
+  getFormatedTime = (dueDate) => {
+    const date = new Date(dueDate);
+    const day = date.getDay();
+    const month = date.toLocaleString('en', { month: 'short' });
+    const formatedTime = month + ' ' + day;
+    return formatedTime;
+  };
+
+  getDueTimeStyle = (card) => {
+    if (card.isComplete) return { backgroundColor: '#61BD4F' };
+    else if (Date.now - card.dueDate <= 0)
+      return { backgroundColor: '#EB5A46' };
+    return { backgroundColor: '#F2D600' };
+  };
+
+  toggleCardComplete=( ev, boardId, groupId, cardId)=>{
+    ev.stopPropagation();
+
+    this.props.onUpdateCard({boardId, groupId, cardId, isComplete: !this.props.card.isComplete }, "isComplete", this.props.board )
+  }
+
   render() {
-    const { card, groupId } = this.props;
-    const {isCardLabelListOpen} =this.state
+    const { board, card, groupId, openCardEdit } = this.props;
+    const { isCardLabelListOpen } = this.state;
     const backgroundColor = card.style
       ? card.style.bgColor
         ? { backgroundColor: card.style.bgColor }
         : {}
       : {};
     return (
-      <div className='card-preview flex space-between'>
+      <div
+        className='card-preview flex space-between'
+        onClick={()=>openCardEdit(board._id, groupId, card.id)}
+      >
         {card.style && (
           <div className='card-preview-header' style={backgroundColor}></div>
         )}
         {card.labelIds && (
-          <ul onClick={this.toggleCardLabelList} className={`label-bar-list flex clean-list ${isCardLabelListOpen ? "open" : "close"}`}>
+          <ul
+            onClick={this.toggleCardLabelList}
+            className={`label-bar-list flex clean-list ${
+              isCardLabelListOpen ? 'open' : 'close'
+            }`}
+          >
             {card.labelIds.map((labelId) => {
               const label = this.getLabel(labelId);
               return (
-                <li className="label-bar" key={labelId} style={{ backgroundColor: label.color }}>
+                <li
+                  className='label-bar'
+                  key={labelId}
+                  style={{ backgroundColor: label.color }}
+                >
                   {isCardLabelListOpen && <span>{label.title}</span>}
                 </li>
               );
             })}
           </ul>
         )}
+
         <p>{card.title}</p>
-        {card.description && <GrTextAlignFull title={"This card has a description"}/>}
+
         <button className='hover-edit-btn'>
           <HiOutlinePencil />
         </button>
-        <div className='card-preview-footer'></div>
+
+        <div className='card-preview-footer flex align-center'>
+          {card.dueDate && (
+            <div className='due-date-box' style={this.getDueTimeStyle(card)} onClick={(event)=>this.toggleCardComplete( event, board._id, groupId, card.id)}>
+              <span className='clock-icon'>
+                <FiClock />
+              </span>
+              <span className='check-icon'>
+                {card.isComplete ? <GrCheckboxSelected /> : <GrCheckbox />}
+              </span>
+              <span>{this.getFormatedTime(card.dueDate)}</span>
+            </div>
+          )}
+
+          {card.description && (
+            <GrTextAlignFull title={'This card has a description'} />
+          )}
+          {card.members && (
+            <div className='card-members-list flex'>
+              {card.members.map((member) => (
+                <MemberAvatar member={member} size={'md'} key={member.id} />
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     );
   }
@@ -62,6 +127,7 @@ function mapStateToProps(state) {
 }
 const mapDispatchToProps = {
   // loadBoard,
+  onUpdateCard
 };
 
 export const CardPreview = connect(
