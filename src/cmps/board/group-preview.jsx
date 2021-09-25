59cdb5b92afe74f8199099779dbb22f040628437
ShirLaV <shir0lavi@gmail.com>
2021-09-25 10:22:51 +0300

    clean input scss

--- src/cmps/board/group-preview.jsx
+++ src/cmps/board/group-preview.jsx
@@ -10,19 +10,45 @@ import { AddBoardItem } from '../shared/add-board-item.jsx';
 export class GroupPreview extends Component {
   state = {
     isAddPopOpen: false,
+    groupTitle: '',
   };
+
+  componentDidMount(){
+    this.setState({...this.state, groupTitle: this.props.group.title})
+  }
 
   onToggleAddPop = () => {
     this.setState({ isAddPopOpen: !this.state.isAddPopOpen });
   };
 
+  handleChange = ({ target: { name, value } }) => {
+    this.setState((prevState) => ({...prevState, [name]: value  }));
+  };
+
+  handlePropertyChange = ({ target: { name, value } }) => {
+    // let dataParams = this.props.match.params;
+    const group= this.props.group;
+    group.title=this.state.groupTitle
+    const action = { type: 'UPDATE_GROUP', group };
+    this.props.onUpdateBoard(action, this.props.board);
+  };
+
   render() {
-    const { group, openCardEdit } = this.props;
-    const { isAddPopOpen } = this.state;
+    const { board, group, openCardEdit } = this.props;
+    const { isAddPopOpen, groupTitle } = this.state;
     return (
       <div className='group-preview'>
         <div className='group-header flex space-between align-center'>
-          <h4>{group.title}</h4>
+          <input
+            className='clean-input'
+            type='text'
+            value={groupTitle}
+            name='groupTitle'
+            onChange={this.handleChange}
+            onBlur={this.handlePropertyChange}
+          />
+
+          {/* <h4>{group.title}</h4> */}
           <button>
             <BsThreeDots />
           </button>
@@ -43,7 +69,10 @@ export class GroupPreview extends Component {
         )}
         <div className='group-footer flex space-between align-center'>
           {!isAddPopOpen && (
-            <button className='add-boarditem-btn flex align-center' onClick={this.onToggleAddPop}>
+            <button
+              className='add-boarditem-btn flex align-center'
+              onClick={this.onToggleAddPop}
+            >
               <i className='flex align-center'>
                 <AiOutlinePlus />
               </i>
