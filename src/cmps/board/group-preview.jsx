f96a5aaabe4dae849497b24daf05ef61865c016c
Anna Haskelsky <annahaskelsky@gmail.com>
2021-09-28 11:05:20 +0300

    added popover to group preview

--- src/cmps/board/group-preview.jsx
+++ src/cmps/board/group-preview.jsx
@@ -8,12 +8,16 @@ import { GrClose } from 'react-icons/gr';
 import { CardPreview } from './card-preview.jsx';
 
 import { AddBoardItem } from '../shared/add-board-item.jsx';
+import { DynamicPopover } from '../shared/dynamic-popover.jsx';
 
 export class GroupPreview extends Component {
   state = {
     isAddPopOpen: false,
     groupTitle: '',
+    isPopoverOpen: false
   };
+
+  groupEditRef = React.createRef()
 
   componentDidMount() {
     this.setState({
@@ -23,7 +27,7 @@ export class GroupPreview extends Component {
   }
 
   componentDidUpdate(prevProps) {
-    if(prevProps.group!==this.props.group){
+    if (prevProps.group !== this.props.group) {
       this.setState({
         ...this.state,
         groupTitle: this.props.group.title,
@@ -57,84 +61,92 @@ export class GroupPreview extends Component {
       index,
       toggleCardComplete
     } = this.props;
-    const { isAddPopOpen, groupTitle } = this.state;
+    const { isAddPopOpen, groupTitle, isPopoverOpen } = this.state;
     // console.log('groupTitle', groupTitle)
     return (
       <div className="group-wrapper">
 
-      <Draggable draggableId={group.id} index={index}>
-        {(provided) => (
-          
-          <div
-            className='group-preview flex column'
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-            ref={provided.innerRef}
-          >
-            <div className='group-header flex space-between align-center'>
-              <input
-                className='clean-input'
-                type='text'
-                value={groupTitle}
-                name='groupTitle'
-                onFocus={this.handleFocus}
-                onChange={this.handleChange}
-                onBlur={this.onChangeGroupTitle}
-              />
-              <button>
-                <BsThreeDots />
-              </button>
-            </div>
-            <Droppable droppableId={group.id}>
-              {(provided) => (
-                <ul
-                  className='card-list clean-list'
-                  {...provided.droppableProps}
-                  ref={provided.innerRef}
-                >
-                  {group.cards &&
-                    group.cards.map((card, index) => {
-                      return (
-                        <CardPreview
-                          key={card.id}
-                          card={card}
-                          index={index}
-                          groupId={group.id}
-                          openCardEdit={openCardEdit}
-                          toggleCardLabelList={toggleCardLabelList}
-                          isCardLabelListOpen={isCardLabelListOpen}
-                          toggleCardComplete={toggleCardComplete}
-                        />
-                      );
-                    })}
-                  {provided.placeholder}
-                  {isAddPopOpen && (
-                    <AddBoardItem
-                      onToggleAddPop={this.onToggleAddPop}
-                      type={'card'}
-                      groupId={group.id}
-                    />
-                  )}
-                </ul>
-              )}
-            </Droppable>
+        <Draggable draggableId={group.id} index={index}>
+          {(provided) => (
 
-            <div className='group-footer flex space-between align-center'>
-              {!isAddPopOpen && (
-                <button
-                  className='add-boarditem-btn flex align-center'
-                  onClick={this.onToggleAddPop}
-                >
-                  <i className='flex align-center'>
-                    <AiOutlinePlus />
-                  </i>
-                  <span>Add a card</span>
-                </button>
-              )}
+            <div
+              className='group-preview flex column'
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+              ref={provided.innerRef}
+            >
+              <div className='group-header flex space-between align-center'>
+                <input
+                  className='clean-input'
+                  type='text'
+                  value={groupTitle}
+                  name='groupTitle'
+                  onFocus={this.handleFocus}
+                  onChange={this.handleChange}
+                  onBlur={this.onChangeGroupTitle}
+                />
+
+                <div className="relative" ref={this.groupEditRef} onClick={() => this.setState({ isPopoverOpen: !isPopoverOpen })}>
+                  <button>
+                    <BsThreeDots />
+                  </button>
+                  {isPopoverOpen && <DynamicPopover onClose={() => this.setState({ isPopoverOpen: false })} ref={this.groupEditRef} title="List actions">
+
+                  </DynamicPopover>}
+                </div>
+
+
+              </div>
+              <Droppable droppableId={group.id}>
+                {(provided) => (
+                  <ul
+                    className='card-list clean-list'
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                  >
+                    {group.cards &&
+                      group.cards.map((card, index) => {
+                        return (
+                          <CardPreview
+                            key={card.id}
+                            card={card}
+                            index={index}
+                            groupId={group.id}
+                            openCardEdit={openCardEdit}
+                            toggleCardLabelList={toggleCardLabelList}
+                            isCardLabelListOpen={isCardLabelListOpen}
+                            toggleCardComplete={toggleCardComplete}
+                          />
+                        );
+                      })}
+                    {provided.placeholder}
+                    {isAddPopOpen && (
+                      <AddBoardItem
+                        onToggleAddPop={this.onToggleAddPop}
+                        type={'card'}
+                        groupId={group.id}
+                      />
+                    )}
+                  </ul>
+                )}
+              </Droppable>
+
+              <div className='group-footer flex space-between align-center'>
+                {!isAddPopOpen && (
+                  <button
+                    className='add-boarditem-btn flex align-center'
+                    onClick={this.onToggleAddPop}
+                  >
+                    <i className='flex align-center'>
+                      <AiOutlinePlus />
+                    </i>
+                    <span>Add a card</span>
+                  </button>
+                )}
+              </div>
             </div>
-          </div>
-        )}
-      </Draggable>
+          )}
+        </Draggable>
       </div>
 
     );
