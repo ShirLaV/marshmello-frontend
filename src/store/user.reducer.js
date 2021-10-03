182071bbc451035e49b5e98d3347d644888c218d
ShirLaV <shir0lavi@gmail.com>
2021-10-03 18:11:32 +0300

    testing

--- src/store/user.reducer.js
+++ src/store/user.reducer.js
@@ -4,16 +4,16 @@ import { userService } from '../services/user.service.js'
 const initialState = {
     user: userService.getLoggedinUser(),
     users: [],
-    watchedUser : null
+    watchedUser: null
 }
 export function userReducer(state = initialState, action) {
     var newState = state;
     switch (action.type) {
         case 'SET_USER':
-            newState = { ...state, user: action.user }
+            newState = {...state, user: action.user }
             break;
         case 'SET_WATCHED_USER':
-            newState = { ...state, watchedUser: action.user }
+            newState = {...state, watchedUser: action.user }
             break;
         case 'REMOVE_USER':
             newState = {
@@ -22,7 +22,7 @@ export function userReducer(state = initialState, action) {
             }
             break;
         case 'SET_USERS':
-            newState = { ...state, users: action.users }
+            newState = {...state, users: action.users }
             break;
         default:
     }
